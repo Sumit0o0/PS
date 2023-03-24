4fc5892ba6b1d5d5a66acb9b9bb29124cdeec99e
Sumit Kumar <owlisfragile@gmail.com>
2023-03-24 15:08:41 +0530

    update...

--- resources/js/index.js
+++ resources/js/index.js
@@ -190,28 +190,28 @@ $(document).ready(function () {
      * @property {array} strings strings to be typed
      * @property {string} stringsElement ID of element containing string children
      */
-    strings: ["Sumit Kumar", "Web Developer"],
+    strings: ["Sumit Kumar", "Web Developer","Sumit Kumar", "Web Developer"],
     stringsElement: null,
 
     /**
      * @property {number} typeSpeed type speed in milliseconds
      */
-    typeSpeed: 50,
+    typeSpeed: 100,
 
     /**
      * @property {number} startDelay time before typing starts in milliseconds
      */
-    startDelay: 100,
+    startDelay: 170,
 
     /**
      * @property {number} backSpeed backspacing speed in milliseconds
      */
-    backSpeed: 300,
+    backSpeed: 150,
 
     /**
      * @property {boolean} smartBackspace only backspace what doesn't match the previous string
      */
-    smartBackspace: true,
+    smartBackspace: false,
 
     /**
      * @property {boolean} shuffle shuffle the strings
@@ -221,7 +221,7 @@ $(document).ready(function () {
     /**
      * @property {number} backDelay time before backspacing in milliseconds
      */
-    backDelay: 200,
+    backDelay: 150,
 
     /**
      * @property {boolean} fadeOut Fade out instead of backspace
@@ -265,3 +265,51 @@ $(document).ready(function () {
     contentType: "html",
   });
 });
+
+
+// Get the canvas node and the drawing context
+const canvas = document.getElementById('canv');
+const ctx = canvas.getContext('2d');
+
+// set the width and height of the canvas
+const w = canvas.width = document.body.offsetWidth;
+const h = canvas.height = document.body.offsetHeight;
+
+// draw a black rectangle of width and height same as that of the canvas
+ctx.fillStyle = '#fff';
+ctx.fillRect(0, 0, w, h);
+
+const cols = Math.floor(w / 20) + 1;
+const ypos = Array(cols).fill(0);
+
+function matrix () {
+  // Draw a semitransparent black rectangle on top of previous drawing
+  ctx.fillStyle = '#fff1';
+  ctx.fillRect(0, 0, w, h);
+
+  // Set color to green and font to 15pt monospace in the drawing context
+  ctx.fillStyle = '#b3ffb3';
+  ctx.font = '15pt monospace';
+
+  // for each column put a random character at the end
+  ypos.forEach((y, ind) => {
+    // generate a random character
+    const text = String.fromCharCode(Math.random() * 128);
+
+    // x coordinate of the column, y coordinate is already given
+    const x = ind * 20;
+    // render the character at (x, y)
+    ctx.fillText(text, x, y);
+
+    // randomly reset the end of the column if it's at least 100px high
+    if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
+    // otherwise just move the y coordinate for the column 20px down,
+    else ypos[ind] = y + 20;
+  });
+}
+
+// render the animation at 20 FPS.
+setInterval(matrix, 50);
+
+
+  
